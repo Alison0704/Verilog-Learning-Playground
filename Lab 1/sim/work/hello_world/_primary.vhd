9eeea1637d3e4f6b59db2b0f89763f2832834df7
Alison Emilien <aemil072@uottawa.ca>
2025-01-26 11:32:07 -0500

    starting again

---
+++ Lab 1/sim/work/hello_world/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity hello_world is
+end hello_world;
---
+++ Lab 2/sum_product.v
@@ -0,0 +1,25 @@
+
+
+// Data Types - Example
+module sum_product();
+  
+  integer a, b;
+  integer sum_int;
+  
+  real x, y;
+  real prod_real;
+  
+  initial begin
+    a = 3;
+    b = 9;
+    sum_int = a + b;
+    $display("\n\t a = %0d, b = %0d, sum = %0d", a, b, sum_int);
+    
+    x = 99.67;
+    y = -33.41;
+    prod_real = x * y;
+    $display("\n\t x = %0.2f, y = %0.2f, prod_real = %0.2f", x, y, prod_real);   
+  end
+  
+endmodule
+
